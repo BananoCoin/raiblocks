1581027dc3e7652f19705474524151edd4875904
coranos <coranos@gmail.com>
2018-04-23 22:22:04 -0500

    merging

--- banano/node/bootstrap.hpp
+++ banano/node/bootstrap.hpp
@@ -89,9 +89,9 @@ public:
 	void add_pull (rai::pull_info const &);
 	bool still_pulling ();
 	void process_fork (MDB_txn *, std::shared_ptr<rai::block>);
-	void try_resolve_fork (MDB_txn *, std::shared_ptr<rai::block>, bool);
-	void resolve_forks ();
 	unsigned target_connections (size_t pulls_remaining);
+	bool should_log ();
+	std::chrono::steady_clock::time_point next_log;
 	std::deque<std::weak_ptr<rai::bootstrap_client>> clients;
 	std::weak_ptr<rai::bootstrap_client> connection_frontier_request;
 	std::weak_ptr<rai::frontier_req_client> frontiers;
@@ -103,7 +103,6 @@ public:
 	std::shared_ptr<rai::node> node;
 	std::atomic<unsigned> account_count;
 	std::atomic<uint64_t> total_blocks;
-	std::unordered_map<rai::block_hash, std::shared_ptr<rai::block>> unresolved_forks;
 	bool stopped;
 	std::mutex mutex;
 	std::condition_variable condition;
@@ -127,13 +126,12 @@ public:
 	rai::account landing;
 	rai::account faucet;
 	std::chrono::steady_clock::time_point start_time;
-	std::chrono::steady_clock::time_point next_report;
 	std::promise<bool> promise;
 };
 class bulk_pull_client : public std::enable_shared_from_this<rai::bulk_pull_client>
 {
 public:
-	bulk_pull_client (std::shared_ptr<rai::bootstrap_client>, rai::pull_info const &, size_t);
+	bulk_pull_client (std::shared_ptr<rai::bootstrap_client>, rai::pull_info const &);
 	~bulk_pull_client ();
 	void request ();
 	void receive_block ();
@@ -143,7 +141,6 @@ public:
 	std::shared_ptr<rai::bootstrap_client> connection;
 	rai::block_hash expected;
 	rai::pull_info pull;
-	size_t size;
 };
 class bootstrap_client : public std::enable_shared_from_this<bootstrap_client>
 {
@@ -192,13 +189,14 @@ public:
 	void notify_listeners (bool);
 	void add_observer (std::function<void(bool)> const &);
 	bool in_progress ();
+	std::shared_ptr<rai::bootstrap_attempt> current_attempt ();
 	void process_fork (MDB_txn *, std::shared_ptr<rai::block>);
 	void stop ();
+
+private:
 	rai::node & node;
 	std::shared_ptr<rai::bootstrap_attempt> attempt;
 	bool stopped;
-
-private:
 	std::mutex mutex;
 	std::condition_variable condition;
 	std::vector<std::function<void(bool)>> observers;
